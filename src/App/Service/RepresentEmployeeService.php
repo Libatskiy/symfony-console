7dfa31684bad4ec42c71a2f4a55f8415a0cf0233
libatskiy <libatsky@gmail.com>
2020-10-13 16:52:20 +0300

    refactoring#

--- src/App/Service/RepresentEmployeeService.php
+++ src/App/Service/RepresentEmployeeService.php
@@ -2,7 +2,7 @@
 
 namespace Console\APP\Service;
 
-use Console\App\Models\EmployeeInterface;
+use Console\App\Models\EmployeeAbstract;
 
 class RepresentEmployeeService
 {
@@ -10,7 +10,7 @@ class RepresentEmployeeService
 
     protected $employee;
 
-    public function __construct(EmployeeInterface $employee)
+    public function __construct(EmployeeAbstract $employee)
     {
         $this->employee = $employee;
     }
